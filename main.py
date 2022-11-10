a6401ae306a4800923002e782e511db2e6abad4d
Ethan Rosenberg <erosenberg2019@hotmail.com>
2022-11-09 23:21:34 -0800

    Added Scryfall query functionality. Also fixed an issue causing unnecessary DB refreshes.

--- main.py
+++ main.py
@@ -1,36 +1,49 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import ImageTk, Image
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
+import urllib.parse
 import io
 import json
 from mergesort import sortTree, merge
 import time
 from operator import itemgetter
+from collections import deque
+import requests
 
 IMG_SIZE = 'normal'
 TYPELINE_WIDTH = 35
 NAME_WIDTH = 50 
 MANA_WIDTH = 15
-URL = 'https://c1.scryfall.com/file/scryfall-cards/png/front/b/7/b768efa2-e56b-4a7e-ace8-d673f10e0714.png?1562880960'
+URL = 'https://cards.scryfall.io/small/front/8/6/86bf43b1-8d4e-4759-bb2d-0b2e03ba7012.jpg?1562242171'
+CI_PADDING = 10
+INPUT_BREAK = 4
 
 # Load full card list from scryfall dump
 oracleFile = open('JSON\oracle-cards.json', 'r', encoding="utf8")
 oracleDB = json.load(oracleFile)
 oracleDB = sorted(oracleDB, key=itemgetter('name')) 
 
-
 # Initialize Tkinter Window
 root = Tk()
 root.resizable(width = 1, height = 1)
+root.minsize(500,300)
 root.title("QuickSilver")
+root.iconbitmap('misc/icon.ico')
+root.columnconfigure(0, weight = 1)
+root.rowconfigure(0, weight = 1)
+root.state('zoomed')
 
 import tkinter as tk
 from tkinter import ttk
 
 #Paned Window to store frames
 pWin = ttk.PanedWindow(root, orient = HORIZONTAL)
-pWin.pack(fill = BOTH, expand = True)
+pWin.grid(column = 0, row = 0, sticky = (N, S, E, W))
+pWin.columnconfigure(0, weight = 8, minsize = 800)
+pWin.rowconfigure(0, weight = 1)
+pWin.columnconfigure(1, weight = 1)
+
 
 style = ttk.Style()
 style.theme_use('classic')
@@ -38,11 +51,15 @@ style.configure("pWin")
 
 # Paned Window to store decklist and cardimg
 rightFrame = ttk.Frame(pWin)
-rightFrame.pack(side = RIGHT, fill = BOTH, expand = True)
-pWin.add(rightFrame)
+rightFrame.grid(column = 1, row = 0, sticky = (N, S, E, W))
+rightFrame.columnconfigure(0, weight = 1)
+rightFrame.rowconfigure(0, weight = 1)
 
 pRight = ttk.PanedWindow(rightFrame, orient = VERTICAL)
-pRight.pack(fill = BOTH, expand = True, side = RIGHT)
+pRight.grid(column = 0, row = 0, sticky = (N, S, E, W))
+pRight.columnconfigure(0, weight = 1)
+pRight.rowconfigure(0, weight = 1)
+pRight.rowconfigure(1, weight = 3)
 
 style = ttk.Style()
 style.theme_use('classic')
@@ -50,103 +67,128 @@ style.configure("pWin")
 
 # Initialize Card Viewer Window
 cardViewer = ttk.Frame(pRight)
-cardViewer.pack(side = TOP, fill = BOTH, expand = True)
+cardViewer.grid(column = 0, row = 1, sticky = (N, S, E, W))
+cardViewer.columnconfigure(0, weight = 1)
+cardViewer.rowconfigure(0, weight = 1)
+cardViewer.rowconfigure(1, weight = 1)
 pRight.add(cardViewer)
 
 # Store Current Card Name
 cardName = StringVar()
 
-def searchDB(cardName):
-    for card in oracleDB:
-        if card["name"].upper() == cardName.upper():
-            return card
+# Create a buffer to store most recent 10 Images
+imgBuff = deque([])
+
+# Bool to switch between simple and scryfall search
+simpleSearch = BooleanVar()
+simpleSearch.set(True)
+
+def searchDB(cardName = False, ID = False):
+    if ID:
+        for card in oracleDB:
+            if card["id"] == ID:
+                return card
+    if cardName:
+        for card in oracleDB:
+            if card["name"].upper() == cardName.upper():
+                return card
     print('Card not found')
     return -1
 
+lastCard = searchDB(cardName = "Birthing Pod")
+
 # Check all listboxes for selected card
-def select(*args):
-    widget = root.focus_get()
+def select(event):
+    global lastCard
+    if event is None:
+        return
+    widget = event.widget
     if type(widget) is ttk.Treeview:
         iid = widget.focus()
         if widget.item(iid)["values"]:
-            displayCard(searchDB(widget.item(iid)["values"][0]))
-    elif type(widget) is ttk.Entry:
-        iid = dbTree.focus()
-        if dbTree.item(iid)["values"]:
-            displayCard(searchDB(dbTree.item(iid)["values"][0]))
+            card = searchDB(ID = widget.item(iid)["values"][0])
+            displayCard(card)
+            lastCard = card
+    else:
+        displayCard(lastCard)
 
 # Initialize decklist
 deck = []
 
 # Return position in deck, -1 if not in deck
-def inDeck(name):
+def inDeck(ID):
     cursor = 0
     for card in deck:
-        if card["CO"]["name"].upper() == name.upper():
+        if card['CO']['id'] == ID:
             return cursor
         cursor = cursor + 1
     return -1
 
-# Reload List Into Display
-def reload(*args):
-    cat1List.delete(*cat1List.get_children())
-    cat12List.delete(0, END)
-    cat13List.delete(0, END)
-    cat2List.delete(0, END)
-    cat23List.delete(0, END)
-    cat3List.delete(0, END)
-    catAllList.delete(0, END)
-    for card in deck:
-        addToDeck(card)
-
 # Define function to add card to decklist
 def addCard(*args):
-    name = cardName.get()
+    if "values" not in dbTree.item(dbTree.focus()):
+        print(f'Failed to add card: {dbTree.item(dbTree.focus())}')
+        return -1
+    
+    cardID = dbTree.item(dbTree.focus())["values"][0]
     # Fetch card location
-    locus = inDeck(name)
+    locus = inDeck(cardID)
     # If card not in deck, add
     if locus == -1:
-        CO = searchDB(name)
+        CO = searchDB(ID = cardID)
         if CO == -1:
             return -1
-        print(CO)
-        deck.append({"CO": CO, "n": 1, "cat1": cat1.get(), "cat2": cat2.get(), "cat3": cat3.get()})
-        displayCard(CO)
+        deck.append({'CO': CO, 'n': 1})
     # If card is in deck, increase quantity
     else:
         current = deck.pop(locus)
         current["n"] = current["n"] + 1
-        current["cat1"] = cat1.get()
-        current["cat2"] = cat2.get()
-        current["cat3"] = cat3.get()
         deck.append(current)
-        displayCard(current["CO"])
     reload()
 
-cardView = ttk.Label(cardViewer)
-cardView.pack(fill = BOTH, expand = True)
+# Reload decklist tree
+def reload():
+    deckTree.delete(*deckTree.get_children())
+    for card in deck:
+        deckTree.insert('', END, values=(card['CO']['id'], card['n'], card['CO']['name']))
 
+cardView = ttk.Label(cardViewer)
+cardView.grid(column = 0, row = 0, sticky = (N, S, E, W))
+
+""" Image enable / disable (temporarily deprecated)
 imgOn = BooleanVar()
 imgOn.set(True)
 
 imgMode = ttk.Checkbutton(cardViewer, text = "Display Images?", variable = imgOn, onvalue = True, offvalue = False)
-imgMode.pack(side = TOP)
-
+imgMode.grid(column = 0, row = 1)
+"""
 
 def convertImg(imgURL):
-    page = urlopen(imgURL)
+    head = {'User-Agent': 'Mozilla/5.0'}
+    page = urlopen(Request(imgURL, headers=head))
     img = io.BytesIO(page.read())
     pilImg = Image.open(img)
     return pilImg
 
-def updateImg(pilImg, w, h):
-    img = pilImg.resize((w, h))
+# Store current width and height to abort pointless re-renders
+curW = 0
+curH = 0
+curPil = convertImg(URL)
+
+def updateImg(pilImg, w, h, resize = False, cardObj = cardView):
+    global curW, curH
+    if (w == curW) and (h == curH) and resize:
+        return
+    
+    img = pilImg.resize((int(w), int(h)))
+    curW = w
+    curH = h
     tkImg = ImageTk.PhotoImage(img)
-    cardView.configure(image = tkImg)
-    cardView.image = tkImg
+    cardObj.configure(image = tkImg)
+    cardObj.image = tkImg
 
 #Display image with new updated width and height
-def displayImg():
+def displayImg(resize = False):
     root.update() # refresh widget info
     
     w = cardViewer.winfo_width()
@@ -157,129 +199,278 @@ def displayImg():
     else:
         w = h*0.71
     
-    updateImg(curPil, int(w), int(h))
+    updateImg(curPil, int(w), int(h), resize)
 
-curPil = convertImg(URL)
-
-lastID = ''
-
+# Update card display
 def displayCard(card):
-    if imgOn.get() == False:
-        print("imgOn no!")
-        return
-    
     global lastID
     global curPil
     
+    resize = False
+    
     ID = card["id"]
 
-    if lastID != ID:
+    # only fetch new image if it's not stored in the buffer
+    if not (ID in imgBuff):
+        if len(imgBuff) != 0:
+            imgBuff.popleft()
+        imgBuff.append(ID)
+        
         if "card_faces" in card:
             curPil = convertImg(card["card_faces"][0]["image_uris"][IMG_SIZE])
         else:
             curPil = convertImg(card["image_uris"][IMG_SIZE])
-    displayImg()
-    lastID = ID
+    else:
+        resize = True
+    
+    displayImg(resize)
 
 displayImg()
 
 #Frame for all card list browsing
 listFrame = ttk.Frame(pWin, padding = 10)
-listFrame.pack(expand = True, fill = BOTH, side = LEFT)
+listFrame.grid(column = 0, row = 0, sticky = (N, S, E, W))
+listFrame.columnconfigure(0, weight = 1)
+listFrame.rowconfigure(1, weight = 1)
 pWin.add(listFrame)
+pWin.add(rightFrame)
 
 #Frame for decklist
 deckFrame = ttk.Frame(pRight, padding = 10)
-deckFrame.pack(expand = True, fill = BOTH)
+deckFrame.grid(column = 0, row = 1, sticky = (N, S, E, W))
+deckFrame.columnconfigure(0, weight = 1)
+deckFrame.columnconfigure(1, minsize = 25)
+deckFrame.rowconfigure(0, weight = 1)
+
 pRight.add(deckFrame)
 
 # Create a frame to store input widgets in
 inFrame = ttk.Frame(listFrame, padding = 10)
-inFrame.pack(side = TOP, fill = X)
-
-
-## Create a frame to store cards in
-#listFrame = ttk.Frame(root, padding = 10)
-#listFrame.grid(column = 1, row = 1)
+inFrame.grid(column = 0, row = 0, sticky = N)
 
 # Create a frame to store full card list in
-dbFrame = ttk.Frame(listFrame, padding = 10)
-dbFrame.pack(side = BOTTOM, fill = BOTH, expand = True)
+dbTreeFrame = ttk.Frame(listFrame, padding = 10)
+dbTreeFrame.columnconfigure(0, weight = 1)
+dbTreeFrame.columnconfigure(1, minsize = 25)
+dbTreeFrame.rowconfigure(0, weight = 1)
+
+def dbListPlace():
+    dbTreeFrame.grid(column = 0, row = 1, sticky = (N, S, E, W))
 
 # Full card list display
-dbTree = ttk.Treeview(dbFrame, columns = ('name', 't', 'mc'), show='headings', selectmode = 'browse')
+dbTree = ttk.Treeview(dbTreeFrame, columns = ('id', 'name', 't', 'mc'), show='headings', selectmode = 'browse')
+dbTree['displaycolumns']=('name', 't', 'mc')
 dbTree.heading('name', text='Name')
 dbTree.heading('t', text='Type')
 dbTree.heading('mc', text='Mana Cost')
-dbTree.pack(side = LEFT, fill = BOTH, expand = True)
-
-dbScroll = ttk.Scrollbar(dbFrame,
-                         orient = "vertical",
+dbTree.grid(column = 0, row = 0, sticky = (N, S, E, W))
+dbScroll = ttk.Scrollbar(dbTreeFrame,
+                         orient = 'vertical',
                          command = dbTree.yview)
-dbScroll.pack(side = RIGHT, fill = BOTH)
+dbScroll.grid(column = 1, row = 0, sticky = (N, S))
 dbTree.configure(yscrollcommand = dbScroll.set)
 
+# Image database display
+dbImgFrame = ttk.Frame(listFrame, padding = 5)
+dbImgFrame.grid(column = 0, row = 1, sticky = (N, S, E, W))
+
+ciWidth = 0
+ciHeight = 0
+ciColCount = 0
+ciRowCount = 0
+ciArr = []
+
+# Calculate format for card image page
+def formatCIPage():
+    global ciWidth, ciColCount
+    DBW = dbImgFrame.winfo_width()
+    if DBW > 1000:
+        # Size for 4 cards
+        ciWidth = (DBW - CI_PADDING*5)*0.25
+        ciColCount = 4
+    elif DBW > 700:
+        # Size for 3 cards
+        ciWidth = (DBW - CI_PADDING*4)*0.33
+        ciColCount = 3
+    else:
+        #Size for 2 cards
+        ciWidth = (DBW - CI_PADDING*4)*0.5
+        ciColCount = 2
+
+# Define array to hold card image objects
+def defCardObjArr():
+    global ciHeight, ciRowCount, ciArr
+    
+    # Purge old card labels
+    for cx in ciArr:
+        for cy in cx:
+            cy.grid_forget()
+            del cy
+    
+    DBH = dbImgFrame.winfo_height()
+    ciHeight = ciWidth*1.4
+    ciRowCount = int((DBH - CI_PADDING) / (ciHeight + 10))
+    
+    totalImg = int(ciColCount * ciRowCount)
+    
+    dbCurLis = dbTree.get_children()
+    
+    if len(dbCurLis) <= 1:
+        return
+    
+    idArray = []
+    
+    ciArr = []
+    
+    for n in range(totalImg):
+        idArray.append(dbTree.item(dbCurLis[n])["values"][0])
+    
+    for ciX in range(ciColCount):
+        ciArr.append([])
+        for ciY in range (ciRowCount):
+            ciArr[ciX].append(ttk.Label(dbImgFrame))
+            ciArr[ciX][ciY].grid(column = ciX, row = ciY)
+            card = searchDB(ID = idArray[ciX + ciY])
+            
+            if "card_faces" in card:
+                pilImg = convertImg(card["card_faces"][0]["image_uris"][IMG_SIZE])
+            else:
+                pilImg = convertImg(card["image_uris"][IMG_SIZE])
+            
+            updateImg(pilImg, ciWidth, ciHeight, cardObj = ciArr[ciX][ciY])
+
+def updateImgDB(*args):
+    formatCIPage()
+    defCardObjArr()
+
+def placeImgDB():
+    dbImgFrame.grid(column = 0, row = 1, sticky = (N, S, E, W))
+    updateImgDB()
+
+
 # Deck list display
-deckTree = ttk.Treeview(deckFrame, columns = ('q', 'name'), show='headings', selectmode = 'browse')
+deckTree = ttk.Treeview(deckFrame, columns = ('id', 'q', 'name'), show='headings', selectmode = 'browse')
+deckTree["displaycolumns"] = ('q', 'name')
 deckTree.heading('q', text='#')
+deckTree.column('q', minwidth = 20, width = 20, stretch = False)
 deckTree.heading('name', text='Name')
-deckTree.pack(side = LEFT, fill = BOTH, expand = True)
+deckTree.grid(column = 0, row = 0, sticky = (N, S, E, W))
 
 deckScroll = ttk.Scrollbar(deckFrame,
                          orient = "vertical",
                          command = deckTree.yview)
-deckScroll.pack(side = RIGHT, fill = BOTH)
+deckScroll.grid(column = 1, row = 0, sticky = (N, S))
 deckTree.configure(yscrollcommand = deckScroll.set)
 
 
 # Load Card DB
 for card in oracleDB:
-    item = [card["name"], card["type_line"]]
+    if 'paper' not in card['games']:
+        continue
+
+    item = [card['id'], card['name'], card['type_line']]
+    if 'mana_cost' in card:
+        item.append(card['mana_cost'])
+    else:
+        item.append('None')
+
+    dbTree.insert('', END, values=item)
+
+treeChildren = dbTree.get_children()
+if treeChildren:
+    top_child = treeChildren[0]
+    dbTree.focus(top_child)
+    dbTree.selection_set(top_child)
+
+# Add card object to DB treelist
+def addDBItem(card):
+    if 'paper' not in card['games']:
+        return -1
+    item = [card['id'], card['name'], card['type_line']]
     if "mana_cost" in card:
-        item.append(card["mana_cost"])
+        item.append(card['mana_cost'])
     else:
         item.append('None')
     dbTree.insert('', END, values=item)
-"""
-t = time.time()
-sortedIid = sortTree(dbTree, dbTree.get_children(), 0)
-print(time.time() - t)
 
-
-cursor = 0
-for iid in sortedIid:
-    dbTree.move(iid, dbTree.parent(iid), cursor)
-    cursor = cursor + 1
-"""
-
-def filterByName(*args):
-    dbTree.delete(*dbTree.get_children())
-    for card in oracleDB:
-        item = [card["name"], card["type_line"]]
-        if "mana_cost" in card:
-            item.append(card["mana_cost"])
-        else:
-            item.append('None')
-        if cardName.get().upper() in card["name"].upper():
-            dbTree.insert('', END, values=item)
-    
+# Select / focus top treelist item
+def selectDBTop():
     treeChildren = dbTree.get_children()
     if treeChildren:
         top_child = treeChildren[0]
         dbTree.focus(top_child)
         dbTree.selection_set(top_child)
 
+def filterByName(*args):
+    dbTree.delete(*dbTree.get_children())
+    for card in oracleDB:
+        if cardName.get().upper() in card['name'].upper():
+            addDBItem(card)
+    selectDBTop()
+
+# returns scryfall search query as json
+def scrySearch(query):
+    str = urllib.parse.quote(query)
+    print("Fetching from scryfall...")
+    response = requests.get('https://api.scryfall.com/cards/search?q='+str)
+    print("...Fetch complete")
+    return response.json()['data']
+# Load scryfall search into tree
+def scryFilter(*args):
+    dbTree.delete(*dbTree.get_children())
+    db = scrySearch(cardName.get())
+    for card in db:
+        addDBItem(card)
+    selectDBTop()
+
+
 # Card Input
 cin = ttk.Entry(inFrame, textvariable=cardName) # Card Name Input
-cin.pack(side = TOP)
+cin.grid(column = 0, row = 0, sticky = W)
+cin.focus_force()
 
-#add = ttk.Button(inFrame, text="Add", command=addCard) # Add To List
-#add.pack(side = LEFT)
+# Button to swap between text DB and image DB
+imgDB = True
+
+def swapDBMode(*args):
+    global imgDB
+    if imgDB:
+        dbImgFrame.grid_forget()
+        dbListPlace()
+        imgDB = False
+    else:
+        dbTreeFrame.grid_forget()
+        placeImgDB()
+        imgDB = True
+
+swapDBMode()
+
+def refresh(event = None):
+    if root.focus_get() == cin and simpleSearch.get():
+        filterByName()
+        select(event)
+        if imgDB:
+            placeImgDB()
+        root.after(1000, refresh)
+
+dbSwapButton = ttk.Button(inFrame, command = swapDBMode, text = "Swab DB Mode")
+dbSwapButton.grid(column = 1, row = 0)
+
+seachMode = ttk.Checkbutton(inFrame, variable = simpleSearch, text = "Simple Search Mode")
+seachMode.grid(column = 2, row = 0)
+
+scryRefresh = ttk.Button(inFrame, command = scryFilter, text = "Refresh Scryfall Query")
+scryRefresh.grid(column = 3, row = 0)
+
+refresh()
 
 # Key Binds
-root.bind('<Key>', lambda *args: [filterByName(), select()])
 root.bind('<Expose>', select)
 root.bind('<Button-1>', select)
+root.bind('<Up>', select)
+root.bind('<Down>', select)
+root.bind('<Return>', addCard)
+dbTree.bind('Double-Button-1', addCard())
 
 # Run Event Loop
 #cardViewer.mainloop()
